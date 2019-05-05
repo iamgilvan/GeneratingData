db03056748c1b82eab666a81c04931a37e81c09e
Gilvan Almeida <g6almeida@hotmail.com>
2019-05-05 14:49:33 -0300

    Alterando o tamanho para preencher a tela

--- rw_visual.py
+++ rw_visual.py
@@ -5,10 +5,14 @@ from models.random_walk import RandomWalk
 # Continua criando novos passeios enquanto o programa estiver ativo
 while True:
     # Cria um passeio aleatório e plota os pontos
-    rw = RandomWalk()
+    rw = RandomWalk(50000)
     rw.fill_walk()
+
+    # Define o tamanho da janela de plotagem
+    plt.figure(dpi=128, figsize=(10, 6))
+
     point_numbers = list(range(rw.num_points))
-    plt.scatter(rw.x_values, rw.y_values, c=point_numbers, cmap=plt.cm.Blues, edgecolor='none', s=15)
+    plt.scatter(rw.x_values, rw.y_values, c=point_numbers, cmap=plt.cm.Blues, edgecolor='none', s=1)
 
     # Enfatiza o primeiro e o último ponto
     plt.scatter(0, 0, c='green', edgecolors='none', s=100)
