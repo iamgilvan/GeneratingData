0aae3ffb2434cd84b0bb2e65e24f7b678364d1ed
Gilvan Almeida <g6almeida@hotmail.com>
2019-05-05 14:28:39 -0300

    Colorindo os pontos

---
+++ rw_visual.py
@@ -0,0 +1,17 @@
+import matplotlib.pyplot as plt
+
+from models.random_walk import RandomWalk
+
+# Continua criando novos passeios enquanto o programa estiver ativo
+while True:
+    # Cria um passeio aleatório e plota os pontos
+    rw = RandomWalk()
+    rw.fill_walk()
+    point_numbers = list(range(rw.num_points))
+    plt.scatter(rw.x_values, rw.y_values, c=point_numbers, cmap=plt.cm.Blues, edgecolor='none', s=15)
+    plt.show()
+
+    keep_running = input("Make another walk? (y/n): ")
+
+    if keep_running == 'n':
+        break
