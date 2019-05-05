af6cb0623117b127ba4af07f03726d2e0d0368e0
Gilvan Almeida <g6almeida@hotmail.com>
2019-05-05 14:39:17 -0300

    Plotando os pontos de início e de fim

--- rw_visual.py
+++ rw_visual.py
@@ -9,6 +9,15 @@ while True:
     rw.fill_walk()
     point_numbers = list(range(rw.num_points))
     plt.scatter(rw.x_values, rw.y_values, c=point_numbers, cmap=plt.cm.Blues, edgecolor='none', s=15)
+
+    # Enfatiza o primeiro e o último ponto
+    plt.scatter(0, 0, c='green', edgecolors='none', s=100)
+    plt.scatter(rw.x_values[-1], rw.y_values[-1], c='red', edgecolors='none', s=100)
+
+    # Remove os eixos
+    #plt.axes().get_xaxis().set_visible(False)
+    #plt.axes().get_yaxis().set_visible(False)
+
     plt.show()
 
     keep_running = input("Make another walk? (y/n): ")
